2a9ea4630526b88e93dc817f27f3668a374e28aa
Eazekil <daimon_95@mail.ru>
2021-02-09 13:30:39 +0300

    Parameters GET added annotation @RequestParam

--- src/main/java/com/nokhrin/springmvc/config/controllers/FirstController.java
+++ src/main/java/com/nokhrin/springmvc/config/controllers/FirstController.java
@@ -3,13 +3,18 @@ package com.nokhrin.springmvc.config.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage(){
+    public String helloPage(@RequestParam(value = "firstName", required = false) String firstName,
+                            @RequestParam(value = "lastName", required = false) String lastName){
+        System.out.println("Hello, "+firstName+" "+lastName);
         return "first/hello";
     }
     @GetMapping("/goodbye")
